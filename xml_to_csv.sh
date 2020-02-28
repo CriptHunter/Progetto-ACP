cc04a0c5c4a5774cf94588bacd09cff2e03c5ef4
CriptHunter <lorenzo.dalessandro1@studenti.unimi.it>
2020-02-28 11:34:47 +0100

    SH script now writes AWK result in CSV file

--- xml_to_csv.sh
+++ xml_to_csv.sh
@@ -1,10 +1,12 @@
-columns=""
-echo "Enter the number of columns : "
-read n_col
+read -p "Enter the filename: " file  # read filename
+fileArray=(`echo $file | tr '.' ' '`); # split it into an array of two elements
+fileNoExt=${fileArray[0]} # filename without extension
 
+read -p "Enter the number of columns : " n_col
 
+# read columns names
 for ((i = 1; i <= n_col; i++)); do
-	read -p "Enter column : " input
+	read -p "Enter column ${i}: " input
 	if (( $i == 1 ));
 	then
 		columns="${input}"
@@ -13,10 +15,9 @@ for ((i = 1; i <= n_col; i++)); do
 	fi
 done
 
+#read the tag that divides elements in the xml file
 read -p "Enter the item divider's name : " input
-itemTag="</${input}>"
+itemTag="</${input}>" 
 
-#columns="name,price,description,calories"
-#itemTag="</food>"
-
-awk -v columns=$columns -v itemTag=$itemTag -f xml_to_csv.awk food.xml
+#call awk script and writes his output to a csv file
+awk -v columns=$columns -v itemTag=$itemTag -f xml_to_csv.awk $file > "${fileNoExt}.csv"
