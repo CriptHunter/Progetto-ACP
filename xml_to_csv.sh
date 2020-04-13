3d8132b492e7763b91255f255098106a6383db79
CriptHunter <lorenzo.dalessandro1@studenti.unimi.it>
2020-04-13 16:22:02 +0200

    fixed bash script to work on linux

--- xml_to_csv.sh
+++ xml_to_csv.sh
@@ -1,3 +1,5 @@
+# Run using bash script_name.sh
+
 read -p "Enter the filename: " file  # read filename
 fileArray=(`echo $file | tr '.' ' '`); # split it into an array of two elements
 fileNoExt=${fileArray[0]} # filename without extension
@@ -5,9 +7,10 @@ fileNoExt=${fileArray[0]} # filename without extension
 read -p "Enter the number of columns : " n_col
 
 # read columns names
-for (i = 1; i <= n_col; i++); do
+for((i = 1; i <= n_col; i++)) 
+do
 	read -p "Enter column ${i}: " input
-	if (( $i == 1 ));
+	if (( $i == 1 ))
 	then
 		columns="${input}"
 	else 
@@ -15,9 +18,9 @@ for (i = 1; i <= n_col; i++); do
 	fi
 done
 
-#read the tag that divides elements in the xml file
+# read the tag that divides elements in the xml file
 read -p "Enter the item divider's name : " input
 itemTag="</${input}>" 
 
-#call awk script and writes his output to a csv file
+# call awk script and writes his output to a csv file
 awk -v columns=$columns -v itemTag=$itemTag -f xml_to_csv.awk $file > "${fileNoExt}.csv"
