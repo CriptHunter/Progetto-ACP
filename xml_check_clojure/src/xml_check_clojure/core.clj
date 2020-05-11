d1593004a4944a8abbcc2237da8c9d87bf44a5ca
CriptHunter <lorenzo.dalessandro1@studenti.unimi.it>
2020-05-11 12:35:20 +0200

    xml check working, need to fix well formed function

--- xml_check_clojure/src/xml_check_clojure/core.clj
+++ xml_check_clojure/src/xml_check_clojure/core.clj
@@ -5,49 +5,64 @@
 
 ;XML file to be parsed
 (def xml_file "food.xml")
+;parsed XML file
 (def xml_file_parsed (xml/parse xml_file))
 
 ;return true if the XML file can be parsed by clojure.xml library
-(defn well-formed?
-  [filename]
+(defn well-formed? [filename]
   (try
     (xml/parse filename)
     true
     (catch Exception e false))
   )
 
-;print a list in a readable way
+;print list elements on separate lines with spacing
 (defn print_list [L]
   (if (empty? L)
     nil
-    (do (print (first L)) (print "\n") (print "\n") (print_list (rest L))))
+    (do (print (first L)) 
+        (print "\n") 
+        (print "\n") 
+        (print_list (rest L))))
   )
 
 ;true if two lists have the same elements in the same position
 (defn equal_list? [L1 L2]
-  (if (empty? L1)
-    true
-    (and (= (first L1) (first L2)) (equal_list? (rest L1) (rest L2)))))
+  (if (not= (count L1) (count L2))
+    false
+    
+    (if (empty? L1)
+      true
+      (and (= (first L1) (first L2))
+           (equal_list? (rest L1) (rest L2)))))
+  )
 
-;return a list of tags given a parsed XML file
+;return a list of tags given a :content in a XML file
 (defn get_tags [L]
   (map :tag L))
 
-;compare all the tags in the inner level of the xml file
+
+;compare all the tags on the same level of a XML file
 (defn compare_tags [L]
-  (if (= (count L) 1)
+  (if (and (<= (count L) 1))
     true
     (and (equal_list?
           (get_tags (:content (first L)))
           (get_tags (:content (second L))))
-          (compare_tags (rest L))
+         (= (:tag (first L)) (:tag (second L)))
+         (compare_tags (rest L))
          ))
   )
 
-(defn -main []
-  (println (well-formed? xml_file))
-  (println (get_tags (:content (nth (:content xml_file_parsed) 4))))
-  (println (equal_list? (get_tags (:content (first (:content xml_file_parsed)))) (get_tags (:content (second (:content xml_file_parsed))))))
-  (println (compare_tags (:content xml_file_parsed)))
-  
+;get depth of XML file to avoid nested tag
+(defn get_depth [L]
+  true
   )
+
+(defn -main []
+  (print "well formed? ")
+  (println (well-formed? xml_file))
+  (print "same tags? ")
+  (println (compare_tags (:content xml_file_parsed)))
+  )
+
